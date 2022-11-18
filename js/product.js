38983a8dfc91efa98340d3e99b77e5ce927257f3
Vanivnaa <74012897+Vanivnaa@users.noreply.github.com>
2022-11-18 11:04:05 +0200

    Update product.js

--- js/product.js
+++ js/product.js
@@ -10,3 +10,30 @@ openPopUp.addEventListener('click', function(e){
 closePopUp.addEventListener('click', () => {
     popUp.classList.remove('active')
 })
+
+
+const openNum = document.getElementById('btn_plus');
+const closeNum = document.getElementById('btn_min');
+const num =document.getElementById('numerosity_2');
+
+openNum.addEventListener('click', function(e){
+    e.preventDefault();
+    num.classList.add('active');
+})
+
+closeNum.addEventListener('click', () => {
+    num.classList.remove('active')
+})
+
+
+const AddedBtn = document.getElementById("btn_to_cart");
+function showwAdded() {
+    const x = document.getElementById("added");
+    x.className = "showw";
+    setTimeout(function(){ x.className = x.className.replace("showw", ""); }, 3000);
+}
+
+AddedBtn.addEventListener('click', ()=>{
+    event.preventDefault();
+    showwAdded();
+})
