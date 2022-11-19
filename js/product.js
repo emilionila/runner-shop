a10e89393c4f1c498ef39be53a038e4bf6404e32
Vanivnaa <74012897+Vanivnaa@users.noreply.github.com>
2022-11-19 20:49:07 +0200

    Update product.js

--- js/product.js
+++ js/product.js
@@ -1,14 +1,14 @@
-const openPopUp = document.getElementById('next_ph');
-const closePopUp = document.getElementById('back_ph');
-const popUp =document.getElementById('product_photo_2');
+const openPh = document.getElementById('next_ph');
+const closePh = document.getElementById('back_ph');
+const ph =document.getElementById('product_photo_2');
 
-openPopUp.addEventListener('click', function(e){
+openPh.addEventListener('click', function(e){
     e.preventDefault();
-    popUp.classList.add('active');
+    ph.classList.add('active');
 })
 
-closePopUp.addEventListener('click', () => {
-    popUp.classList.remove('active')
+closePn.addEventListener('click', () => {
+    ph.classList.remove('active')
 })
 
 
